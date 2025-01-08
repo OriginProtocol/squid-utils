8c057f1e6800603dc118195b4738a5aba038b1b0
Chris Jacobs <apexearth@gmail.com>
2025-01-08 15:29:38 -0800

    attempt to take some util code out of origin-squid to be shared

---
+++ src/calculateBlockRate.ts
@@ -0,0 +1,17 @@
+import { arbitrum, base, mainnet } from 'viem/chains'
+
+import { Context } from './processor'
+
+const blockRates: Record<number, number> = {
+  [mainnet.id]: 12.04,
+  [arbitrum.id]: 0.25,
+  [base.id]: 2,
+}
+
+export const calculateBlockRate = async (ctx: Context) => {
+  const rate = blockRates[ctx.chain.id]
+  if (rate) {
+    return rate
+  }
+  throw new Error('No block rate found')
+}
