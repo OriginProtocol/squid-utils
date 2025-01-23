acd57796149f27405a17a0c656770de072d4e532
Chris Jacobs <apexearth@gmail.com>
2025-01-23 15:56:30 -0800

    Add block rate for Sonic chain

--- src/calculateBlockRate.ts
+++ src/calculateBlockRate.ts
@@ -1,4 +1,4 @@
-import { arbitrum, base, mainnet } from 'viem/chains'
+import { arbitrum, base, mainnet, sonic } from 'viem/chains'
 
 import { Context } from './types'
 
@@ -6,6 +6,7 @@ const blockRates: Record<number, number> = {
   [mainnet.id]: 12.04,
   [arbitrum.id]: 0.25,
   [base.id]: 2,
+  [sonic.id]: 0.6,
 }
 
 export const calculateBlockRate = async (ctx: Context) => {
