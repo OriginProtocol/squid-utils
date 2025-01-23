9f562e4b4146f202b260978106ed3ee9674d594d
Chris Jacobs <apexearth@gmail.com>
2025-01-23 14:40:01 -0800

    allow null for second jsonify arg

--- src/jsonify.ts
+++ src/jsonify.ts
@@ -8,7 +8,7 @@ function replacer(key: string, value: unknown) {
 
 export const jsonify = (
   value: any,
-  customReplacer?: (key: string, value: unknown) => unknown,
+  customReplacer?: null | ((key: string, value: unknown) => unknown),
   indent?: number,
 ) =>
   JSON.stringify(value, (key, value) =>
